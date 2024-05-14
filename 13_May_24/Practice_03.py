eeef166c194a1ae40cca12c817fd727e2023facc
MdNizamuddin7865 <nizam.rec34@gmail.com>
2024-05-14 11:30:07 +0530

    This is for Practice_Git hub

---
+++ 13_May_24/Practice_03.py
@@ -0,0 +1,4 @@
+name = input ("Enter your name")
+print ("This is your name",name)
+name = input ("This is your phone number, add the list of contents")
+print ("This is the list",name)
---
+++ 13_May_24/Practice_1.py
@@ -0,0 +1,18 @@
+print("Hello World")
+print("nizam","Shabeeha","Ritesh",end="_")
+print("nizam","Shabeeha","Ritesh")
+result = max(3,4,5)
+print(result)
+age = 78
+age = 87
+print(type(age))
+pi = 3.142
+gst = .018
+print(type(pi))
+import keyword
+a = 1+7j
+b = 8-5
+print(type(a))
+name = "nizam"
+name1 = "ritesh"
+print(type(name))
